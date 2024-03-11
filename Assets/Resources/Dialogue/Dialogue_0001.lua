9fa866c672700b0faec4629cd89d2d566bd2f03e
chiwawaRIN <109030823+chiwawaRIN@users.noreply.github.com>
2024-03-11 19:41:51 +0900

    Dialogue0001~0007

--- Assets/Resources/Dialogue/Dialogue_0001.lua
+++ Assets/Resources/Dialogue/Dialogue_0001.lua
@@ -1,9 +1,9 @@
 Dialogue_0001 = CreateDialog(function()
 
-    Talk("A", "한 방울도 안 남기고 다 먹었구나...`d1` 잘 했어. <size=20>맛 없어서 빨리 처리해버리려고 했는데 다행이야.")
-    Talk("B", "사실 나는 <color=red>`s2`빨간 글씨</color>가 정말 좋아^^")
+    Talk("A", "<size=60>…CODE:ALTER,`d1`, 가동을 시작합니다.")
+    Talk("A", "개체 탈출, 개체 탈출! 연구원들은 신속히 제압 바람!")
 
-    Talk("C", "한 방울도 안 남기고 다 먹었구나...`d1` 잘 했어. <size=20>맛 없어서 빨리 처리해버리려고 했는데 다행이야.")
-    Talk("DDFDSAFDSA", "사실 나는 <color=red>`s2`빨간 글씨</color>가 정말 좋아^^")
+    --Talk("C", "한 방울도 안 남기고 다 먹었구나...`d1` 잘 했어. <size=20>맛 없어서 빨리 처리해버리려고 했는데 다행이야.")
+    --Talk("DDFDSAFDSA", "사실 나는 <color=red>`s2`빨간 글씨</color>가 정말 좋아^^")
 
 end)
